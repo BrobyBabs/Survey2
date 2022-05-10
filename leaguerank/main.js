973945769a25e7d200cc45750ebc272f36795460
brob <bbabs3@hotmail.com>
2022-05-10 02:28:46 -0500

    First Roadblock
    As of 051022 02:28

--- leaguerank/main.js
+++ leaguerank/main.js
@@ -1,3 +1,6 @@
+
+const answers = new URLSearchParams(window.location.search);
+
 function updateSliderValue(sliderID, valueID){
     //store slider value in the variable called sliedrValue
     let sliderValue = document.getElementById(sliderID).value;
@@ -7,19 +10,37 @@ function updateSliderValue(sliderID, valueID){
 
 function evaluateResult(){
     // TODO: write some code to calculate the user input
-    return 40;
+    var total;
+    var creeps = answers.get('csm');
+    total += creeps;
+    var kpp = answers.get('kpp');
+    total += kpp;
+    var wardScore = answers.get('wards');
+    total += wardScore;
+    var goldPer = answers.get('goldearned');
+    total += goldPer;
+    var kda = answers.get('kda');
+    total += kda;
+    var objControl = answers.get('objectives');
+    total += objControl;
+    var teamFightDurability = answers.get('tfd');
+    total += teamFightDurability;
+    var cons = answers.get('consistency');
+    total += cons;
+    console.log(total)
+    return total;
 }
 
 function storeData(){
-    let result = evaluateResult();
-    window.localStorage.setItem('score', result);
+    let score = evaluateResult();
+    window.localStorage.setItem('score', score);
 }
 
 function readData(){
     //get the score from localstorage
     let result = window.localStorage.getItem('score');
     //pass the data to DOM
-    document.getElementById('result').innerHTML = result;
+    document.getElementById('score').innerHTML = result;
 }
 
 // var slider = document.getElementById("cs");
